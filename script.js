565fe186130a376b97603f83f656e48f172e3a58
maite <maite.sanchez@gm2dev.com>
2024-10-18 10:21:51 -0300

    feat(script.js): add anagram function

--- script.js
+++ script.js
@@ -26,6 +26,17 @@
     console.log(sum(1)('a'));
 
 
+/* 3) Escribir una funcion que determine si 2 strings son un anagrama
+    Ignorar espacios en los strings
+    No diferenciar entre mayusculas y minusculas
+    Retornar true si es anagrama y false de lo contrario */
+
+    function anagram(string1, string2){
+        return string1.toLowerCase().split("").sort().join("") === string2.toLowerCase().split("").sort().join("");
+    }
+
+    console.log(anagram("ab", "ba"));
+
       
       
       
